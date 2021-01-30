037a4041d321968f456bf66b7c2c895d02ece39f
maxdexter <maxdexter@bk.ru>
2021-01-30 14:33:30 +0300

    добавил NotificationReceiver.kt

---
+++ app/src/main/java/ru/maxdexter/mytasks/alarm/NotificationReceiver.kt
@@ -0,0 +1,24 @@
+package ru.maxdexter.mytasks.alarm
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import ru.maxdexter.mytasks.R
+
+class NotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        val notification = NotificationCompat.Builder(context, "MyChannel")
+            .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+            .setContentTitle(intent.getStringExtra("title"))
+            .setContentText(intent.getStringExtra("time"))
+            .build()
+
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(84,notification)
+
+
+    }
+}
